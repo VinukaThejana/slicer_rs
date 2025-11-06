d9bcd82b0f0fa341ccc80abb4f27a7ce15e2573b
Vinuka Kodituwakku <vinuka.t@pm.me>
2025-11-06 19:25:51 +0530

    feat(error): added the ability to add custom error messages with the error

--- src/error.rs
+++ src/error.rs
@@ -3,125 +3,194 @@ use axum::{
     http::{StatusCode, header},
     response::{IntoResponse, Response},
 };
-use std::fmt::Display;
 use validator::ValidationErrors;
+
+pub trait StdErrorExt: std::error::Error + Send + Sync + 'static {}
+impl<T> StdErrorExt for T where T: std::error::Error + Send + Sync + 'static {}
 
 #[derive(Debug, thiserror::Error)]
 pub enum AppError {
-    NotFound(String),
-    BadRequest(String),
-    Conflict(String),
-    UniqueViolation(String),
-    Unauthorized(String),
+    #[error("{user_message}")]
+    BadRequest {
+        user_message: String,
+        #[source]
+        source: Option<anyhow::Error>,
+    },
+
+    #[error("{user_message}")]
+    NotFound {
+        user_message: String,
+        #[source]
+        source: Option<anyhow::Error>,
+    },
+
+    #[error("{user_message}")]
+    Conflict {
+        user_message: String,
+        #[source]
+        source: Option<anyhow::Error>,
+    },
+
+    #[error("{user_message}")]
+    UniqueViolation {
+        user_message: String,
+        #[source]
+        source: Option<anyhow::Error>,
+    },
+
+    #[error("{user_message}")]
+    Unauthorized {
+        user_message: String,
+        #[source]
+        source: Option<anyhow::Error>,
+    },
+
+    #[error("{0}")]
     Validation(#[from] ValidationErrors),
 
     #[error(transparent)]
     Other(#[from] anyhow::Error),
 }
 
-impl Display for AppError {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        match self {
-            Self::NotFound(e) => write!(f, "{}", e),
-            Self::BadRequest(e) => write!(f, "{}", e),
-            Self::Conflict(e) => write!(f, "{}", e),
-            Self::UniqueViolation(e) => write!(f, "{}", e),
-            Self::Unauthorized(e) => write!(f, "{}", e),
-            Self::Validation(e) => {
-                let message = e
-                    .field_errors()
-                    .values()
-                    .flat_map(|e| e.iter())
-                    .flat_map(|err| {
-                        err.message
-                            .as_ref()
-                            .map(|msg| msg.to_string())
-                            .or(Some(String::from("invalid value")))
-                    })
-                    .next()
-                    .unwrap_or(String::from("invalid value"));
+impl AppError {
+    pub fn bad_request(msg: impl Into<String>) -> Self {
+        Self::BadRequest {
+            user_message: msg.into(),
+            source: None,
+        }
+    }
 
-                write!(f, "{}", message)
-            }
-            Self::Other(e) => write!(f, "{}", e),
+    pub fn bad_request_with_source<E>(msg: impl Into<String>, err: E) -> Self
+    where
+        E: StdErrorExt,
+    {
+        Self::BadRequest {
+            user_message: msg.into(),
+            source: Some(anyhow::Error::new(err)),
+        }
+    }
+
+    pub fn not_found(msg: impl Into<String>) -> Self {
+        Self::NotFound {
+            user_message: msg.into(),
+            source: None,
+        }
+    }
+
+    pub fn not_found_with_source<E>(msg: impl Into<String>, err: E) -> Self
+    where
+        E: StdErrorExt,
+    {
+        Self::NotFound {
+            user_message: msg.into(),
+            source: Some(anyhow::Error::new(err)),
+        }
+    }
+
+    pub fn conflict(msg: impl Into<String>) -> Self {
+        Self::Conflict {
+            user_message: msg.into(),
+            source: None,
+        }
+    }
+
+    pub fn conflict_with_source<E>(msg: impl Into<String>, err: E) -> Self
+    where
+        E: StdErrorExt,
+    {
+        Self::Conflict {
+            user_message: msg.into(),
+            source: Some(anyhow::Error::new(err)),
+        }
+    }
+
+    pub fn unauthorized(msg: impl Into<String>) -> Self {
+        Self::Unauthorized {
+            user_message: msg.into(),
+            source: None,
+        }
+    }
+
+    pub fn unauthorized_with_source<E>(msg: impl Into<String>, err: E) -> Self
+    where
+        E: StdErrorExt,
+    {
+        Self::Unauthorized {
+            user_message: msg.into(),
+            source: Some(anyhow::Error::new(err)),
         }
     }
 }
 
 impl AppError {
-    pub fn from_generic_error<E>(e: E) -> Self
+    pub fn from_generic_error<E>(err: E) -> Self
     where
-        E: std::error::Error + Send + Sync + 'static,
+        E: StdErrorExt,
     {
-        Self::Other(e.into())
+        Self::Other(anyhow::Error::new(err))
     }
 
-    pub fn from_not_found<E>(e: E) -> Self
-    where
-        E: std::error::Error + Send + Sync + 'static,
-    {
-        Self::NotFound(e.to_string())
-    }
-}
-
-impl AppError {
-    pub fn not_found(message: impl Into<String>) -> Self {
-        Self::NotFound(message.into())
-    }
-
-    pub fn bad_request(message: impl Into<String>) -> Self {
-        Self::BadRequest(message.into())
-    }
-
-    pub fn conflict(message: impl Into<String>) -> Self {
-        Self::Conflict(message.into())
-    }
-
-    pub fn unauthorized(message: impl Into<String>) -> Self {
-        Self::Unauthorized(message.into())
+    fn source_error(&self) -> Option<&anyhow::Error> {
+        match self {
+            AppError::BadRequest { source, .. }
+            | AppError::NotFound { source, .. }
+            | AppError::Conflict { source, .. }
+            | AppError::UniqueViolation { source, .. }
+            | AppError::Unauthorized { source, .. } => source.as_ref(),
+            _ => None,
+        }
     }
 }
 
 impl IntoResponse for AppError {
     fn into_response(self) -> Response {
-        let (status, message) = match self {
-            AppError::NotFound(msg) => {
-                log::info!("[not_found] {}", msg);
-                (StatusCode::NOT_FOUND, msg)
+        let (status_code, tag, user_message) = match &self {
+            AppError::BadRequest { user_message, .. } => {
+                (StatusCode::BAD_REQUEST, "bad_request", user_message)
             }
-            AppError::BadRequest(msg) => {
-                log::info!("[bad_request] {}", msg);
-                (StatusCode::BAD_REQUEST, msg)
+            AppError::NotFound { user_message, .. } => {
+                (StatusCode::NOT_FOUND, "not_found", user_message)
             }
-            AppError::Conflict(msg) => {
-                log::info!("[conflict] {}", msg);
-                (StatusCode::CONFLICT, msg)
+            AppError::Conflict { user_message, .. } => {
+                (StatusCode::CONFLICT, "conflict", user_message)
             }
-            AppError::UniqueViolation(msg) => {
-                log::info!("[unique_violation] {}", msg);
-                (StatusCode::CONFLICT, msg)
+            AppError::UniqueViolation { user_message, .. } => {
+                (StatusCode::CONFLICT, "unique_violation", user_message)
             }
-            AppError::Unauthorized(msg) => {
-                log::info!("[unauthorized] {}", msg);
-                (StatusCode::UNAUTHORIZED, msg)
+            AppError::Unauthorized { user_message, .. } => {
+                (StatusCode::UNAUTHORIZED, "unauthorized", user_message)
             }
-            AppError::Validation(err) => {
-                let err_msg = AppError::Validation(err).to_string();
-                log::info!("[validation] {}", err_msg);
-                (StatusCode::BAD_REQUEST, err_msg)
+            AppError::Validation(errs) => {
+                let msg = errs
+                    .field_errors()
+                    .values()
+                    .flat_map(|v| v.iter())
+                    .flat_map(|e| e.message.as_ref().map(|m| m.to_string()))
+                    .next()
+                    .unwrap_or_else(|| "invalid value".to_string());
+
+                (StatusCode::BAD_REQUEST, "validation", &msg.to_string())
             }
-            AppError::Other(err) => {
-                log::error!("[internal_error] {:?}", err);
-                (StatusCode::INTERNAL_SERVER_ERROR, err.to_string())
-            }
+            AppError::Other(_) => (
+                StatusCode::INTERNAL_SERVER_ERROR,
+                "other",
+                &String::from("something went wrong"),
+            ),
         };
 
+        match (tag == "other", self.source_error()) {
+            (true, Some(source)) => log::error!("[{}] {}: {:?}", tag, user_message, source),
+            (true, None) => log::error!("[{}] {}", tag, user_message),
+            (false, Some(source)) => log::info!("[{}] {}: {:?}", tag, user_message, source),
+            (false, None) => log::info!("[{}] {}", tag, user_message),
+        }
+
         (
-            status,
+            status_code,
             [(header::CONTENT_TYPE, "application/json")],
             Json(serde_json::json!({
                 "status": "error",
-                "message": message,
+                "message": user_message
             })),
         )
             .into_response()
