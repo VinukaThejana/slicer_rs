73c2531b46bafad99116a9c05995383db9c2bb0b
Vinuka Kodituwakku <vinuka.t@pm.me>
2025-11-16 01:08:23 +0530

    feat(calculation): added polygon triangulation logic

--- src/calculate/mod.rs
+++ src/calculate/mod.rs
@@ -1,5 +1,46 @@
+pub mod triangulation;
+
 use crate::model;
 use rayon::prelude::*;
+
+#[derive(Clone, Copy, Debug)]
+pub struct Vec3(pub f32, pub f32, pub f32);
+
+#[derive(Clone, Copy, Debug)]
+pub struct Vec2(pub f32, pub f32);
+
+impl Vec3 {
+    pub fn substraction(self, other: Vec3) -> Vec3 {
+        Vec3(self.0 - other.0, self.1 - other.1, self.2 - other.2)
+    }
+
+    pub fn cross(self, other: Vec3) -> Vec3 {
+        Vec3(
+            // basically the determinant of a 3x3 matrix
+            self.1 * other.2 - self.2 * other.1,
+            self.2 * other.0 - self.0 * other.2,
+            self.0 * other.1 - self.1 * other.0,
+        )
+    }
+
+    pub fn dot(self, other: Vec3) -> f32 {
+        self.0 * other.0 + self.1 * other.1 + self.2 * other.2
+    }
+}
+
+impl Vec2 {
+    pub fn substraction(self, other: Vec2) -> Vec2 {
+        Vec2(self.0 - other.0, self.1 - other.1)
+    }
+
+    pub fn cross(self, other: Vec2) -> f32 {
+        self.0 * other.1 - self.1 * other.0
+    }
+
+    pub fn dot(self, other: Vec2) -> f32 {
+        self.0 * other.0 + self.1 * other.1
+    }
+}
 
 pub fn volume(triangles: &[model::Triangle]) -> f64 {
     if triangles.is_empty() {
@@ -10,10 +51,7 @@ pub fn volume(triangles: &[model::Triangle]) -> f64 {
     const CHUNK_SIZE: usize = 1000;
 
     let total_volume: f64 = if triangles.len() >= PARALLEL_THRESHOLD {
-        triangles
-            .par_chunks(CHUNK_SIZE)
-            .map(|chunk| khan_sum(chunk))
-            .sum()
+        triangles.par_chunks(CHUNK_SIZE).map(khan_sum).sum()
     } else {
         khan_sum(triangles)
     };
